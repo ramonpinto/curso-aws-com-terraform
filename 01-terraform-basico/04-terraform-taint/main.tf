ca4382fb111172ea0e913c9f8dc5eb36af6960b0
Ramon Pinto <ramonaraujopinto@gmail.com>
2021-01-04 14:04:39 -0300

    aulas de terraform

--- 01-terraform-basico/04-terraform-taint/main.tf
+++ 01-terraform-basico/04-terraform-taint/main.tf
@@ -1,19 +1,20 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "sa-east-1"
+  profile = "lab"
 }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-12312312312"
+  bucket = "curso-terraform2020"
   acl    = "private"
 
   tags = {
     Name        = "My bucket"
-    Environment = "Dev"
+    Environment = "PRD"
   }
 }
 
 resource "aws_s3_bucket_object" "object" {
-  bucket = "${aws_s3_bucket.b.id}"
+  bucket = "aws_s3_bucket.b.id"
   key    = "hello-world.txt"
   source = "arquivo.txt"
 }
